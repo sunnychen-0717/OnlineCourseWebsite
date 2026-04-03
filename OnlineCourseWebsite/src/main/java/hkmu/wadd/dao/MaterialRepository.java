aa5674a639f53699c53ea7473817dfd3a61931d9
sunnychen-0717 <s1382229@live.hkmu.edu.hk>
2026-04-04 00:00:26 +0800

    Delete OnlineCourseWebsite directory

--- OnlineCourseWebsite/src/main/java/hkmu/wadd/dao/MaterialRepository.java
+++
@@ -1,4 +0,0 @@
-package hkmu.wadd.dao;
-import hkmu.wadd.model.Material;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface MaterialRepository extends JpaRepository<Material, Long> {}
--- OnlineCourseWebsite/src/main/java/hkmu/wadd/dao/OptionRepository.java
+++
@@ -1,4 +0,0 @@
-package hkmu.wadd.dao;
-import hkmu.wadd.model.Option;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface OptionRepository extends JpaRepository<Option, Long> {}
